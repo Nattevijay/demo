64c32dc1b95d009958be6878f91d04a45fd47133
Vijay <n.vijay8187@gmail.com>
2026-03-13 09:47:40 +0530

    Order service implemented

---
+++ order-service/src/main/java/demo/orderservice/controller/OrderController.java
@@ -0,0 +1,16 @@
+package demo.orderservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+
+    @GetMapping
+    public String orders(){
+        return "Orders working";
+    }
+
+}
---
+++ order-service/src/main/java/demo/orderservice/entity/Order.java
@@ -0,0 +1,24 @@
+package demo.orderservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long productId;
+
+    private int quantity;
+
+}
